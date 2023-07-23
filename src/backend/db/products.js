8ca83339decc8e04f87464266cd8ee82a0e40779
Sandesh9834 <78275234+Sandesh9834@users.noreply.github.com>
2023-07-24 03:11:06 +0530

    feat - filter products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -11,23 +11,39 @@ export const products = [
     title: "Adidas Shoes",
     rating: 5,
     price: 5000,
-    categoryName: "men",
+    categoryName: "Men",
+    image : "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/10533a775dfe4c60852caf8d011b6270_9366/solderrun-m.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Adidas Shoes 2",
+    rating: 5,
+    price: 3000,
+    categoryName: "Men",
+    image : "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/10533a775dfe4c60852caf8d011b6270_9366/solderrun-m.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Adidas Shoes 1",
+    rating: 4,
+    price: 2000,
+    categoryName: "Women",
     image : "https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/10533a775dfe4c60852caf8d011b6270_9366/solderrun-m.jpg"
   },
   {
     _id: uuid(),
     title: "Mountain Bike Rockrider ST50 - Black",
-    rating: 1,
+    rating: 2,
     price: 3000,
-    categoryName: "women",
+    categoryName: "Kids",
     image : "https://contents.mediadecathlon.com/p1836363/09444f07e84fb37d0fc52d417e084de8/p1836363.jpg?format=auto&quality=70&f=650x0"
   },
   {
     _id: uuid(),
     title: "Cross Trainer EL520B (2022)",
     rating: 2,
-    price: 1000,
-    categoryName: "women",
+    price: 10000,
+    categoryName: "Women",
     image : "https://contents.mediadecathlon.com/p2340283/e76d77a4150ad426e03e61f25581371d/p2340283.jpg?format=auto&quality=70&f=650x0"
   },
 ];
--- src/pages/productListing/productListing.jsx
+++ src/pages/productListing/productListing.jsx
@@ -2,34 +2,132 @@ import React, { useEffect, useState } from 'react';
 
 import { Navigation } from '../../components/Navigation';
 import ProductCard from '../../components/ProductCard';
-import FilterComponent from '../../components/FilterComponent';
+// import FilterComponent from '../../components/FilterComponent';
 import './productListing.css';
 
 export default function ProductListingPage() {
   const [productData, setProductData] = useState([]);
+  const [categoryData, setCategoryData] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
+  const [sortByPrice, setSortByPrice] = useState([])
+  const [rating, setRating] = useState(0);
+  const [category, setCategory] = useState(false)
 
   const fetchData = async () => {
     const response = await fetch('/api/products');
     const data = await response.json();
-    console.log(data);
     setProductData(data.products);
+    setFilteredProducts(data.products)
   };
-  
+
+  const fetchCategories = async () => {
+    const response = await fetch("/api/categories")
+    const data = await response.json()
+    setCategoryData(data.categories)
+  }
+
+  const handleCategory = (event) => {
+    const category = event.target.value;
+    setCategory(category);
+    filterProducts(category, rating, sortByPrice);
+  };
+
+  const handleRating = (event) => {
+    const rating = event.target.value
+    setRating(parseInt(rating))
+    filterProducts(category, rating, sortByPrice)
+  }
+
+  const handlePriceSorting = (event) => {
+    const sortByPrice = event.target.value
+    setSortByPrice(sortByPrice)
+    filterProducts(category, rating, sortByPrice)
+  }
+
+  const filterProducts = (category, rating, sortByPrice) => {
+    let filteredData = productData;
+
+    if (category !== "") {
+      filteredData = filteredData.filter((product) => product.categoryName === category);
+    }
+    
+    if (rating !== 0) {
+      filteredData = filteredData.filter(
+        (product) => product.rating >= Number(rating)
+      );
+    }
+
+    if (sortByPrice === "lowToHigh") {
+      filteredData = filteredData.sort((a, b) => a.price - b.price)
+    }
+
+    if (sortByPrice === "highToLow") {
+      filteredData = filteredData.sort((a, b) => b.price - a.price)
+    }
+
+    setFilteredProducts(filteredData);
+  };
+
   useEffect(() => {
     fetchData();
+    fetchCategories();
   }, []);
-
 
   return (
     <>
       <Navigation />
       <div className="main-body-sec">
-        <FilterComponent />
+        <div className="filter-sidebar">
+          <div className="filter-sidebar-heading">
+            <p><b>Filters</b></p>
+            <p>clear</p>
+          </div>
+          <div className="filter-price">
+            <h2 className="price-heading">Rating</h2>
+            <div className="range">
+              <span>0</span>
+              <span>5</span>
+            </div>
+            <input
+              type="range"
+              min="0"
+              max="5"
+              value={rating}
+              className="price-range"
+              onChange={handleRating}
+            />{rating}
+          </div>
+          <div className="filter-cat">
+            <p><b>Category</b></p>
+            <div className="filter-item">
+              <ul className="filter-item">
+                {
+                  categoryData.map((category) =>
+                    <li style={{ listStyle: "none" }} key={category._id}>
+                      <input type="checkbox" value={category.categoryName} name="category" onClick={handleCategory} />
+                      <label>{category.categoryName}</label>
+                    </li>
+                  )}
+              </ul>
+            </div>
+          </div>
+          <div className="filter-cat">
+            <span><b>Price</b></span>
+            <div className="filter-item">
+              <input type="radio" value="lowToHigh" id="lowToHigh" name='priceRange' onChange={handlePriceSorting} />
+              <label for="lowToHigh">Price - Low to High</label>
+            </div>
+            <div className="filter-item">
+              <input type="radio" id="highToLow" value="highToLow" name='priceRange' onChange={handlePriceSorting} />
+              <label for="highToLow">Price - High to Low</label>
+            </div>
+          </div>
+        </div>
         <div className="right-body-section">
           <h1 className="showing-heading">Showing all products</h1>
           <div className="product-flex">
             <>
-              {productData.length > 0 && productData.map((item) =>
+              {filteredProducts.length > 0 && filteredProducts.map((item) =>
                 <ProductCard {...item} />
               )
               }
